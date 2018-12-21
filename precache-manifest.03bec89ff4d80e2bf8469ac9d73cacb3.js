b7801483e561cc0160547e6615e1d06f5ce3d022
wael-zoaiter <wael.syr.95@gmail.com>
2018-12-21 19:17:47 +0200

    Updates

--- precache-manifest.03bec89ff4d80e2bf8469ac9d73cacb3.js
+++ precache-manifest.03bec89ff4d80e2bf8469ac9d73cacb3.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/daily-world/static/js/runtime~main.20f8867b.js"
   },
   {
-    "revision": "823720f9c1ea5b57042a",
-    "url": "/daily-world/static/js/main.823720f9.chunk.js"
+    "revision": "fb2dab5b0794cd9354c5",
+    "url": "/daily-world/static/js/main.fb2dab5b.chunk.js"
   },
   {
     "revision": "720bb394f984eefc212e",
     "url": "/daily-world/static/js/1.720bb394.chunk.js"
   },
   {
-    "revision": "082dfbe5414cca75843b8cb848153e35",
+    "revision": "7ab7c1055098589e8e63bbe1a30de419",
     "url": "/daily-world/index.html"
   }
 ];
